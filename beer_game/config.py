2fc1b8978ff6d419ab0a13fd997e02e931bd3b8d
N Y <steelpipe75@gmail.com>
2026-02-14 19:14:37 +0900

    config見直し

--- beer_game/config.py
+++ beer_game/config.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class GameConfig:
-    delivery_weeks: int = 4
+    delivery_weeks: int = 2
     out_of_stock_penalty: int = 2
     inventory_penalty: int = 1
-    init_inventory: int = 4
+    init_inventory: int = 12
 
 
 CONFIG = GameConfig()
